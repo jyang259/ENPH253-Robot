c46873d68bd80e7d0130c598077f7c98fc83ed0f
Jenny Yang <yangjy259@alumni.ubc.ca>
2016-07-04 15:48:05 -0700

    Updates on Arm/Claw actuation code

--- clawCode.cpp
+++ clawCode.cpp
@@ -1,44 +1,53 @@
 /*
 Code for claw mechanism 
+1. Arm turns to face direction of passenger
+2. Arm lowers to level of passenger
+3. Arm extends forward until passenger is within reach
+4. Claw closes around passenger
 */
 
 #include <Servo.h>
 
-int servoArm = 1 //Servo for telescoping arm
-int servoClaw = 2 //Servo for claw mechanism 
+int microSwitch = 1//Microswitch for passenger detection
 
-int microSwitch = 2//Microswitch for passenger detection
+Servo servoArm; 
+Servo servoClaw;
 
-Servo servo; 
+int servoArmPin = 2;
+int servoClawPin = 3;
 
-int pos = 0; //servo position in degrees
+int posArm = 0; //servo position in degrees
 
 void setup() {
   // put your setup code here, to run once:
-  servo.attach(servoPin);
+  servoArm.attach(servoArmPin);
+  servoClaw.attach(servoClawPin);
   pinMode(microSwitch, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //scan from 0 to 180 degrees
   
-  /*for(pos = 0; pos < 180; pos++){
-    servo.write(pos);
-    delay(15);
-  }
-  
-  //scan back from 180 to 0 degrees
-  for(pos = 180; pos > 0; pos--){
-    servo.write(pos);
-    delay(15);
-  }*/
-  
-  //microswitch
-  if(digitalRead(microSwitch) == HIGH){
-    for(pos=180; pos < 90; pos--){
-      
+  //Initial position of arm and claw
+  servoClaw.write(180); //Claw starts opened 
+  servoArm.write(0); //Arm starts contracted
+
+  //ARM ACTUATION
+  //when IR detected within certain strength, extend arm forward (in direction of passenger), 
+  //until microswitch pushed in (i.e. passenger is pressed against claw)
+  if(/*TODO: if IR detection is at certain strength*/){
+    while(digitalRead(microSwitch) == LOW && posArm < 180){
+      servoArm.write(posArm++);
     }
+  } 
+
+  //CLAW ACTUATION
+  if(digitalRead(microSwitch) == HIGH){
+    //When microswitch is on high, close the claw around passenger
+    for(posClaw = 180; posArm < 90; posClaw--){
+      servoClaw.write(posClaw);
+    }
+    //TODO: When destination beacon reached, drop off passenger 
   }
   
 }
